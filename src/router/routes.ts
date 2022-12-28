2db56b0c986870a2c8c3c0e22967069a3b220613
lsy <siyu.long@qq.com>
2022-12-28 20:59:29 +0800

    first commit

---
+++ src/router/routes.ts
@@ -0,0 +1,23 @@
+import { RouteRecordRaw } from 'vue-router';
+
+const routes: RouteRecordRaw[] = [
+  {
+    path: '/',
+    component: () => import('layouts/MainLayout.vue'),
+    children: [
+      { path: '', component: () => import('pages/IndexPage.vue') },
+      { path: 'page1', component: () => import('pages/Page1.vue') },
+      { path: 'page2', component: () => import('pages/Page2.vue') },
+      { path: 'page3', component: () => import('pages/Page3.vue') },
+    ],
+  },
+
+  // Always leave this as last one,
+  // but you can also remove it
+  {
+    path: '/:catchAll(.*)*',
+    component: () => import('pages/ErrorNotFound.vue'),
+  },
+];
+
+export default routes;
