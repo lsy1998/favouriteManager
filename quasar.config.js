9d9dd0172c4833f65220448024a357cde141feb9
lsy <siyu.long@qq.com>
2023-01-15 16:14:21 +0800

    使用quasar的notify插件

--- quasar.config.js
+++ quasar.config.js
@@ -25,6 +25,7 @@ module.exports = configure(function (/* ctx */) {
     boot: [
       'i18n',
       'axios',
+      'main',
     ],
 
     // https://v2.quasar.dev/quasar-cli-vite/quasar-config-js#css
@@ -118,7 +119,11 @@ module.exports = configure(function (/* ctx */) {
 
     // https://v2.quasar.dev/quasar-cli-vite/quasar-config-js#framework
     framework: {
-      config: {},
+      config:
+      {
+        notify: { /* look at QuasarConfOptions from the API card */ }
+    }
+      ,
 
       // iconSet: 'material-icons', // Quasar icon set
       // lang: 'en-US', // Quasar language pack
@@ -131,7 +136,9 @@ module.exports = configure(function (/* ctx */) {
       // directives: [],
 
       // Quasar plugins
-      plugins: []
+      plugins: [
+        'Notify'
+      ]
     },
 
     // animations: 'all', // --- includes all animations
